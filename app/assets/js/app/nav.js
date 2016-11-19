0f43043896cf8aeab572a7523a48e0622c9470f9
Rafael Cetina <rafael.cetina@hotmail.com>
2016-11-19 17:18:46 -0500

    v1.0

--- app/assets/js/app/nav.js
+++ app/assets/js/app/nav.js
@@ -9,10 +9,13 @@ $('a.link').click(function(e){
     if(str!= $(this).attr('href')){
 	   ajaxLoad($(this).attr('href'));
     }
+
+    // console.log($(this).context.text);
+    breadcrumb = $(this).context.text;
 });
 
 function ajaxLoad(filename, content) {
-    //NProgress.start();
+    NProgress.start();
     content = typeof content !== 'undefined' ? content : 'content';
     
     $.ajax({
@@ -20,14 +23,19 @@ function ajaxLoad(filename, content) {
         url: filename,
         contentType: false,
         success: function (data) {
+            
+            $(".mega-menu").removeClass('open');
+
+            $("#breadcrumb").html(breadcrumb);
+
             $("#" + content).html(data);
             
-            window.history.replaceState(data, "Menu::P", filename);
-            //NProgress.done();
+            //window.history.pushState(data, "Menu::P", filename);
+            NProgress.done();
         },
         error: function (xhr, status, error) {
             alert(xhr.responseText);
-            //NProgress.done();
+            NProgress.done();
         }
     });
 }
